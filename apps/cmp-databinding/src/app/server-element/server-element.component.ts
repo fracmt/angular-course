29073f90d291326550cf1c3dd72f32814ba90f63
Francisco Torres <fracmt@gmail.com>
2021-01-17 13:50:17 +0000

    Get access to ng-content with ContentChild

--- apps/cmp-databinding/src/app/server-element/server-element.component.ts
+++ apps/cmp-databinding/src/app/server-element/server-element.component.ts
@@ -13,6 +13,7 @@ import {
   OnDestroy,
   ViewChild,
   ElementRef,
+  ContentChild,
 } from "@angular/core";
 
 @Component({
@@ -34,6 +35,7 @@ export class ServerElementComponent
   @Input("srvElement") element: { type: string; name: string; content: string };
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() {
     console.log("constructor called!");
@@ -47,6 +49,7 @@ export class ServerElementComponent
   ngOnInit() {
     console.log("ngOnInit called!");
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -55,6 +58,7 @@ export class ServerElementComponent
 
   ngAfterContentInit() {
     console.log("ngAfterContentInit called!");
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
