c72c23cb116f6adf475f2abac86bc1d663b95c69
Francisco Torres <fracmt@gmail.com>
2021-01-28 23:00:43 +0000

    Adding Ingredients to Recipes

--- apps/recipe-book/src/app/recipes/recipe.service.ts
+++ apps/recipe-book/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/Ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -8,8 +9,12 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
-    ),
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    )
   ];
 
   getRecipes() {
