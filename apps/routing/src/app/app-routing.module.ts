a28acfbfaa53107d18104d10f9a0f157351bf017
Francisco Torres <fracmt@gmail.com>
2021-02-01 20:14:48 +0000

    Protect Child Nested Routes with canActivateChild

--- apps/routing/src/app/app-routing.module.ts
+++ apps/routing/src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const appRoutes: Routes = [
   },
   {
     path: "servers",
-    canActivate: [AuthGuard],
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
       { path: ":id", component: ServersComponent },
--- apps/routing/src/app/auth-guard.service.ts
+++ apps/routing/src/app/auth-guard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
 } from "@angular/router";
@@ -9,20 +10,26 @@ import { Observable } from "rxjs/Observable";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService,
-    private router: Router) {}
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.authService.isAuthenticated().then(
-        (authenticated: boolean) => {
-          if(authenticated) {
-            return true;
-          } else {
-            this.router.navigate(['/']);
-          }
-        }
-      )
-    }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(["/"]);
+      }
+    });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
+  }
 }
