4a7539aea3491378bbbb1f99d418102b10f1c257
Francisco Torres <fracmt@gmail.com>
2021-02-08 21:18:01 +0000

    Passing Dynamic Parameters to Links

--- apps/recipe-book/src/app/recipes/recipe.service.ts
+++ apps/recipe-book/src/app/recipes/recipe.service.ts
@@ -1,7 +1,7 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { Ingredient } from "../shared/Ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
@@ -12,17 +12,18 @@ export class RecipeService {
       'A Test Recipe',
       'This is a simply a test',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    )
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
   ];
 
   constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
