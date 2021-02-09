9c2df481f8ca38713c87ca0bc1714da1ea2b9d44
Francisco Torres <fracmt@gmail.com>
2021-02-09 17:33:17 +0000

    Errors & Completion

--- apps/observables/src/app/home/home.component.ts
+++ apps/observables/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { interval, Subscription } from "rxjs";
+import { isRegExp } from "util";
 
 @Component({
   selector: "app-home",
@@ -20,12 +21,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if(count === 2) {
+          observer.complete();
+        }
+        if(count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000);
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
