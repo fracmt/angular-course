5a1610bd9f89d0e5853fe977c36b9dd41fc00eac
Francisco Torres <fracmt@gmail.com>
2021-02-09 20:28:31 +0000

    Understanding Operators

--- apps/observables/src/app/home/home.component.ts
+++ apps/observables/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { interval, Subscription } from "rxjs";
-import { isRegExp } from "util";
+import { Observable, Subscription } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -17,28 +16,41 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSubscription = interval(1000).subscribe((count) => {
     //   console.log(count);
     // });
-    const customIntervalObservable = Observable.create(observer => {
+    const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if(count === 2) {
+        if (count === 8) {
           observer.complete();
         }
-        if(count > 3) {
-          observer.error(new Error('Count is greater 3!'));
+        if (count > 10) {
+          observer.error(new Error("Count is greater 3!"));
         }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    }, error => {
-      console.log(error);
-      alert(error.message);
-    }, () => {
-      console.log('Completed!');
-    });
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          return +data % 2 == 0;
+        }),
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log("Completed!");
+        }
+      );
   }
 
   ngOnDestroy() {
