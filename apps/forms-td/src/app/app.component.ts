79342121a949ef1bd1734d864160c81355eeb9bc
Francisco Torres <fracmt@gmail.com>
2021-02-12 19:19:54 +0000

    Using Form Data

--- apps/forms-td/src/app/app.component.ts
+++ apps/forms-td/src/app/app.component.ts
@@ -10,18 +10,31 @@ export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
   defaultQuestion = "teacher";
   answer = "";
-  genders = ['male', 'female'];
+  genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
     this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName
-      }
-    })
+        username: suggestedName,
+      },
+    });
   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
