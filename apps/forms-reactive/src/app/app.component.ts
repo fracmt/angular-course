7eff3b8038f6d86e039c54c54d6b61e845085d8e
Francisco Torres <fracmt@gmail.com>
2021-02-20 19:48:38 +0000

    Reactive - Setting and Patching Values

--- apps/forms-reactive/src/app/app.component.ts
+++ apps/forms-reactive/src/app/app.component.ts
@@ -30,10 +30,24 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (value) => console.log(value)
     );
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'fracmt',
+        'email': 'fracmt@test.com',
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna',
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
