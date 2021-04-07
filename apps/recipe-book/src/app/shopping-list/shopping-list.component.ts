5ebefc0f13da37417e4682eefd94dcf7d5aa9883
Francisco Torres <fracmt@gmail.com>
2021-04-07 18:57:47 +0100

    Selecting State

--- apps/recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ apps/recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
 
 import { Ingredient } from '../shared/Ingredient.model';
@@ -11,21 +12,23 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
   private igChangeSub: Subscription;
 
   constructor(
     private slService: ShoppingListService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
   ) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.igChangeSub = this.slService.ingredientsChanged.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // );
 
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
   }
@@ -35,6 +38,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.igChangeSub.unsubscribe();
+    //this.igChangeSub.unsubscribe();
   }
 }
--- apps/recipe-book/src/app/shopping-list/store/shopping-list.reducer.ts
+++ apps/recipe-book/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,22 +1,24 @@
 import { Action } from '@ngrx/store';
 
-import { Ingredient } from "../../shared/Ingredient.model";
+import { Ingredient } from '../../shared/Ingredient.model';
 
-import * as ShoppingListActions from './shopping-list.actions'
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
-  ingredients: [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
-}
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
+};
 
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
-  switch(action.type) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
+  switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        Ingredients: [...state.ingredients, action.payload]
+        Ingredients: [...state.ingredients, action.payload],
       };
+    default:
+      return state;
   }
 }
