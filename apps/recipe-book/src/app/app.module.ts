f12c12c0def28a90b302d58b3b14dd92ff4aa9fd
Francisco Torres <fracmt@gmail.com>
2021-04-07 18:06:31 +0100

    Setting Up the NgRx Store

--- apps/recipe-book/src/app/app.module.ts
+++ apps/recipe-book/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AlertComponent } from './shared/alert/alert.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -18,6 +20,7 @@ import { CoreModule } from './core.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
     SharedModule,
     CoreModule,
   ],
