129d1eb9d82eecd332cd5a501110e5bda8fcb666
Francisco Torres <fracmt@gmail.com>
2021-04-07 12:15:53 +0100

    More Lazy Loading

--- apps/recipe-book/src/app/app.module.ts
+++ apps/recipe-book/src/app/app.module.ts
@@ -7,10 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AlertComponent } from './shared/alert/alert.component';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -20,8 +18,6 @@ import { AuthModule } from './auth/auth.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule,
   ],
