04fe1fd2b261651729e57b07c0024110316e40de
Francisco Torres <fracmt@gmail.com>
2022-05-13 13:50:58 +0100

    Preparing other Auth Actions

--- apps/recipe-book/src/app/auth/store/auth.effects.ts
+++ apps/recipe-book/src/app/auth/store/auth.effects.ts
@@ -22,6 +22,10 @@ export interface AuthResponseData {
 export class AuthEffects {
   apiKey = '';
 
+  authSignup = this.actions$.pipe(
+    ofType(AuthActions.SIGNUP_START)
+  )
+
   @Effect()
   authLogin = this.actions$.pipe(
     ofType(AuthActions.LOGIN_START),
@@ -41,7 +45,7 @@ export class AuthEffects {
             const expirationDate = new Date(
               new Date().getTime() + +resData.expiresIn * 1000
             );
-            return new AuthActions.Login({
+            return new AuthActions.AuthenticateSuccess({
               email: resData.email,
               userId: resData.localId,
               token: resData.idToken,
@@ -51,7 +55,7 @@ export class AuthEffects {
           catchError((errorRes) => {
             let errorMessage = 'An unknown error occurred!';
             if (!errorRes.error || !errorRes.error.error) {
-              return of(new AuthActions.LoginFail(errorMessage));
+              return of(new AuthActions.AuthenticateFail(errorMessage));
             }
             switch (errorRes.error.error.message) {
               case 'EMAIL_EXISTS':
@@ -64,7 +68,7 @@ export class AuthEffects {
                 errorMessage = 'This password is not correct.';
                 break;
             }
-            return of(new AuthActions.LoginFail(errorMessage));
+            return of(new AuthActions.AuthenticateFail(errorMessage));
           })
         );
     })
@@ -72,7 +76,7 @@ export class AuthEffects {
 
   @Effect({ dispatch: false })
   authSuccess = this.actions$.pipe(
-    ofType(AuthActions.LOGIN),
+    ofType(AuthActions.AUTHENTICATE_SUCCESS),
     tap(() => {
       this.router.navigate(['/']);
     })
